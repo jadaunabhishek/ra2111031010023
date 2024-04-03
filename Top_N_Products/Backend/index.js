ca6457ab92c5ed33adbc09540dde63d186139fd9
Abhishek Jadaun <am3239@srmist.edu.in>
2024-04-03 20:46:33 +0530

    RA2111031010023

--- Top_N_Products/Backend/index.js
+++ Top_N_Products/Backend/index.js
@@ -4,6 +4,7 @@ const app = express();
 
 app.use(express.json());
 
+// send the request to register
 app.post('/test/register', (req, res) => {
 
     console.log('Received data:', req.body);
@@ -14,9 +15,11 @@ app.post('/test/register', (req, res) => {
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
+  // print the server is running
     console.log(`Server is running on port ${port}`);
 });
 
+// data to post
 const postData ={
     "companyName": "SRMIST-Abhishek",
     "ownerName": "Abhishek",
@@ -26,15 +29,16 @@ const postData ={
     };
 
 
-// axios.post('http://20.244.56.144/test/register', postData)
-//     .then(response => {
-//         console.log('Response:', response.data);
-//     })
-//     .catch(error => {
-//         console.error('Error:', error);
-//     });
+axios.post('http://20.244.56.144/test/register', postData)
+    .then(response => {
+        console.log('Response:', response.data);
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
 
 
+    // authenticate data
     const authCompanyData ={
         "companyName": "SRMIST-Abhishek",
         "clientID": "21eb4371-b5a2-463d-a415-d54e6d6aa089",
@@ -54,6 +58,7 @@ axios.post('http://20.244.56.144/test/auth', authCompanyData)
     });
 
 
+    // authtoken that we received from the server
 const authToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiZXhwIjoxNzEyMTU0OTI3LCJpYXQiOjE3MTIxNTQ2MjcsImlzcyI6IkFmZm9yZG1lZCIsImp0aSI6IjIxZWI0MzcxLWI1YTItNDYzZC1hNDE1LWQ1NGU2ZDZhYTA4OSIsInN1YiI6ImFtMzIzOUBzcm1pc3QuZWR1LmluIn0sImNvbXBhbnlOYW1lIjoiU1JNSVNULUFiaGlzaGVrIiwiY2xpZW50SUQiOiIyMWViNDM3MS1iNWEyLTQ2M2QtYTQxNS1kNTRlNmQ2YWEwODkiLCJjbGllbnRTZWNyZXQiOiJzcVNMU3dzT2pCTGtGSkV2Iiwib3duZXJOYW1lIjoiQWJoaXNoZWsiLCJvd25lckVtYWlsIjoiYW0zMjM5QHNybWlzdC5lZHUuaW4iLCJyb2xsTm8iOiJSQTIxMTEwMzEwMTAwMjMifQ.-1lpRDwI4-SQvRI5LtJZFMh2ztPkNxPwHh5aRMxiJ1c'
 
 
